fb035091a411708c35d567014322dab5ffed0634
michaelbwalker <mike@alrb.org>
2024-05-21 08:09:18 -0400

    prefinal adds 20240521b
    prefinal adds 20240521b

--- 9. Aggregating/7. pivottable_to_dataframe.py
+++ 9. Aggregating/7. pivottable_to_dataframe.py
@@ -4,6 +4,7 @@ pd.set_option('display.width', 200)
 pd.set_option('display.max_columns', 12)
 pd.set_option('display.max_rows', 200)
 pd.options.display.float_format = '{:,.0f}'.format
+
 coviddaily = pd.read_csv("data/coviddaily.csv",
   parse_dates=["casedate"])
 ltbrazil = pd.read_csv("data/ltbrazil.csv")
